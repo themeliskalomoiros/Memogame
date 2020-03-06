790b8bb173f3983ad48de03d51ffc61a730f7fda
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-03-06 18:59:31 +0100

    init commit

---
+++ app/src/main/java/sk3m3l1io/kalymnos/memogame/MainActivity.java
@@ -0,0 +1,14 @@
+package sk3m3l1io.kalymnos.memogame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
