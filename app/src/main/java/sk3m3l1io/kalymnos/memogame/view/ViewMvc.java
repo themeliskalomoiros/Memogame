38dad609012606af8911cc7a8d11e2be47d27a17
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-03-06 19:41:30 +0100

    Created ViewMvc interface

---
+++ app/src/main/java/sk3m3l1io/kalymnos/memogame/view/ViewMvc.java
@@ -0,0 +1,14 @@
+package sk3m3l1io.kalymnos.memogame.view;
+
+import android.view.View;
+
+public interface ViewMvc {
+    /*
+     * Get the root Android View which is used internally by this MVC View for
+     * presenting data to the user.
+     * The returned Android View might be used by an MVC Controller in order to query
+     * or alter the properties of either the root Android View itself, or any of its
+     * child Android View's.
+     */
+    View getRootView();
+}
