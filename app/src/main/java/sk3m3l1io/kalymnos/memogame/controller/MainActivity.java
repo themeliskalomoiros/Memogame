472daf556a25068d507ca69ed35670de4f112c34
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-03-23 23:41:01 +0100

    Created PlayActivity

--- app/src/main/java/sk3m3l1io/kalymnos/memogame/controller/MainActivity.java
+++ app/src/main/java/sk3m3l1io/kalymnos/memogame/controller/MainActivity.java
@@ -2,15 +2,22 @@ package sk3m3l1io.kalymnos.memogame.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import sk3m3l1io.kalymnos.memogame.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Button play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        play = findViewById(R.id.play);
+        play.setOnClickListener(view -> {
+            Intent i = new Intent();
+        });
     }
 }
---
+++ app/src/main/java/sk3m3l1io/kalymnos/memogame/controller/PlayActivity.java
@@ -0,0 +1,16 @@
+package sk3m3l1io.kalymnos.memogame.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sk3m3l1io.kalymnos.memogame.R;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
