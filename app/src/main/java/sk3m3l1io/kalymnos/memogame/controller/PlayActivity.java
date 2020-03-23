d43874d0ab3be99540d4c45c01de632da7408790
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-03-24 00:22:07 +0100

    PlayActivity uses PlayScreenView

--- app/src/main/java/sk3m3l1io/kalymnos/memogame/controller/PlayActivity.java
+++ app/src/main/java/sk3m3l1io/kalymnos/memogame/controller/PlayActivity.java
@@ -5,12 +5,24 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import sk3m3l1io.kalymnos.memogame.R;
+import sk3m3l1io.kalymnos.memogame.pojos.Game;
+import sk3m3l1io.kalymnos.memogame.view.PlayScreenView;
+import sk3m3l1io.kalymnos.memogame.view.PlayScreenViewImp;
 
-public class PlayActivity extends AppCompatActivity {
+public class PlayActivity extends AppCompatActivity implements PlayScreenView.SymbolClickListener {
+    private PlayScreenView view;
+    private Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play);
+        view = new PlayScreenViewImp(getLayoutInflater(), null);
+        view.setSymbolClickListener(this);
+        setContentView(view.getRootView());
+    }
+
+    @Override
+    public void onSymbolClick(int position) {
+
     }
 }
