8bbfc473c1f38cddc0a19a315d270c62366c3bdc
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-03-26 00:14:22 +0100

    GameProcedure is no longer involved with time scheduling, just reporting matching and game completion

--- app/src/main/java/sk3m3l1io/kalymnos/memogame/controller/GameFragment.java
+++ app/src/main/java/sk3m3l1io/kalymnos/memogame/controller/GameFragment.java
@@ -1,7 +1,119 @@
 package sk3m3l1io.kalymnos.memogame.controller;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class GameFragment extends Fragment {
+import sk3m3l1io.kalymnos.memogame.R;
+import sk3m3l1io.kalymnos.memogame.pojos.Game;
+import sk3m3l1io.kalymnos.memogame.services.GameProcedure;
+import sk3m3l1io.kalymnos.memogame.view.GameView;
+import sk3m3l1io.kalymnos.memogame.view.GameViewImpl;
 
+public class GameFragment extends Fragment implements
+        GameView.SymbolClickListener,
+        GameProcedure.MatchListener,
+        GameProcedure.CompletionListener {
+    private Game game;
+    private GameView view;
+    private GameProcedure gameProcedure;
+    private GameCompletionListener gameCompletionListener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            gameCompletionListener = (GameCompletionListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(getActivity().toString()
+                    + " must implement " + GameCompletionListener.class.getSimpleName());
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameProcedure = new GameProcedure(GameView.SYMBOL_COUNT);
+        gameProcedure.setMatchListener(this);
+        gameProcedure.setCompletionListener(this);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        view = new GameViewImpl(inflater, container);
+        view.setSymbolClickListener(this);
+        view.setAllSymbolsValue(game.getCover());
+        return view.getRootView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        gameProcedure.detachListeners();
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void onMatch(int pos1, int pos2) {
+        view.disableSymbol(pos1);
+        view.disableSymbol(pos2);
+        int symbolColor = getResources().getColor(R.color.symbolMatchColor);
+        view.setSymbolColor(pos1, symbolColor);
+        view.setSymbolColor(pos2, symbolColor);
+    }
+
+    @Override
+    public void onMatchFailure(int position1, int position2) {
+        Runnable setSymbolValues = () -> {
+            int symbolColor = getResources().getColor(R.color.primaryColor);
+            view.setSymbolColor(position1, symbolColor);
+            view.setSymbolColor(position2, symbolColor);
+            view.setSymbolValue(position1, game.getCover());
+            view.setSymbolValue(position2, game.getCover());
+        };
+        runWithDelay(setSymbolValues, 300);
+    }
+
+    public void runWithDelay(Runnable runnable, int delayMillis) {
+        final Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            runnable.run();
+        }, delayMillis);
+    }
+
+    @Override
+    public void onGameCompleted() {
+        view.disableAllSymbols();
+        int color = getResources().getColor(R.color.primaryDarkColor);
+        view.setAllSymbolsBackgroundColor(color);
+        gameCompletionListener.onGameCompleted();
+    }
+
+    @Override
+    public void onSymbolClick(int pos) {
+        int color = getResources().getColor(R.color.secondaryColor);
+        view.setSymbolColor(pos, color);
+        String symbol = game.getSymbols()[pos];
+        view.setSymbolValue(pos, symbol);
+
+        gameProcedure.addTappedSymbol(pos, symbol);
+    }
+
+    public interface GameCompletionListener {
+        void onGameCompleted();
+    }
 }
--- app/src/main/java/sk3m3l1io/kalymnos/memogame/services/GameProcedure.java
+++ app/src/main/java/sk3m3l1io/kalymnos/memogame/services/GameProcedure.java
@@ -1,68 +1,35 @@
 package sk3m3l1io.kalymnos.memogame.services;
 
-import android.os.CountDownTimer;
-
 import androidx.annotation.Nullable;
 
 import java.util.Stack;
 
-public class GameProcedure {
-    private static final int PAIR = 2;
-    private static final int TICK_DURATION = 100;
+public final class GameProcedure {
+    private static final int MATCH_COUNT = 2;
 
-    private final CountDownTimer timer;
-    private final int duration;
-    private TimeListener timeListener;
-    private PairMatchListener pairMatchListener;
-    private ResultListener resultListener;
+    private MatchListener matchListener;
+    private CompletionListener completionListener;
 
     private final int symbolCount;
     private int pairsFound;
     private final Stack<Symbol> pair;
 
-    public GameProcedure(int symbolCount, int durationMilli) {
+    public GameProcedure(int symbolCount) {
         this.symbolCount = symbolCount;
-        duration = durationMilli;
         pair = new Stack<>();
-        timer = new CountDownTimer(duration, TICK_DURATION) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                timeListener.onGameTimeProgress((int) millisUntilFinished);
-            }
-
-            @Override
-            public void onFinish() {
-                timeListener.onGameTimeFinish();
-            }
-        };
     }
 
-    public int getDuration() {
-        return duration;
+    public void setMatchListener(MatchListener matchListener) {
+        this.matchListener = matchListener;
     }
 
-    public void setTimeListener(TimeListener listener) {
-        this.timeListener = listener;
-    }
-
-    public void setPairMatchListener(PairMatchListener pairMatchListener) {
-        this.pairMatchListener = pairMatchListener;
-    }
-
-    public void setResultListener(ResultListener resultListener) {
-        this.resultListener = resultListener;
-    }
-
-    public void resetState() {
-        timer.cancel();
-        pairsFound = 0;
-        pair.clear();
+    public void setCompletionListener(CompletionListener completionListener) {
+        this.completionListener = completionListener;
     }
 
     public void detachListeners() {
-        resultListener = null;
-        pairMatchListener = null;
-        timeListener = null;
+        completionListener = null;
+        matchListener = null;
     }
 
     public void addTappedSymbol(int position, String value) {
@@ -72,14 +39,13 @@ public class GameProcedure {
             pair.push(s);
         }
 
-        if (pair.size() == PAIR) {
+        if (pair.size() == MATCH_COUNT) {
             reportMatch();
             pair.clear();
         }
 
         if (gameWon()) {
-            pairsFound = 0;
-            resultListener.onGameWon();
+            completionListener.onGameCompleted();
         }
     }
 
@@ -87,44 +53,26 @@ public class GameProcedure {
         Symbol s1 = pair.pop();
         Symbol s2 = pair.pop();
 
-        boolean pairMatch = s1.value.equals(s2.value);
-        if (pairMatch) {
-            pairMatchListener.onPairMatch(s1.position, s2.position);
+        if (s1.value.equals(s2.value)) {
+            matchListener.onMatch(s1.position, s2.position);
             pairsFound++;
         } else {
-            pairMatchListener.onPairMatchFail(s1.position, s2.position);
+            matchListener.onMatchFailure(s1.position, s2.position);
         }
     }
 
-    public boolean gameWon() {
+    private boolean gameWon() {
         return pairsFound == symbolCount / 2;
     }
 
-    public void begin() {
-        timer.start();
-        timeListener.onGameTimeBegin();
+    public interface MatchListener {
+        void onMatch(int position1, int position2);
+
+        void onMatchFailure(int position1, int position2);
     }
 
-    public void stop() {
-        timer.cancel();
-    }
-
-    public interface TimeListener {
-        void onGameTimeBegin();
-
-        void onGameTimeProgress(int elapsedMilli);
-
-        void onGameTimeFinish();
-    }
-
-    public interface PairMatchListener {
-        void onPairMatch(int position1, int position2);
-
-        void onPairMatchFail(int position1, int position2);
-    }
-
-    public interface ResultListener {
-        void onGameWon();
+    public interface CompletionListener {
+        void onGameCompleted();
     }
 
     private class Symbol {
