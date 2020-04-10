722e5a78011e36c19b3e9e9bbfc5f603fc662f21
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-04-10 22:52:07 +0200

    Init view inside GameFragment.onCreateView()

--- app/src/main/java/sk3m3l1io/duisburg/memogame/controller/GameEngineFragment.java
+++ app/src/main/java/sk3m3l1io/duisburg/memogame/controller/GameEngineFragment.java
@@ -1,9 +1,18 @@
 package sk3m3l1io.duisburg.memogame.controller;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import sk3m3l1io.duisburg.memogame.game_engine.GameState;
+import sk3m3l1io.duisburg.memogame.pojos.Game;
 import sk3m3l1io.duisburg.memogame.view.game.GameView;
+import sk3m3l1io.duisburg.memogame.view.game.GameViewImpl;
 
 public class GameEngineFragment extends Fragment
         implements GameView.SymbolClickListener,
@@ -35,5 +44,16 @@ public class GameEngineFragment extends Fragment
     @Override
     public void onSymbolAlreadyUncovered(int position) {
 
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        view = new GameViewImpl(inflater, container);
+        view.setSymbolClickListener(this);
+        return view.getRootView();
     }
 }
