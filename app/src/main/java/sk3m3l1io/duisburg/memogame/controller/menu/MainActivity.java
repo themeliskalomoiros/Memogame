64b004eb6f360319d4fc876ed9bb5a4e1db4fb35
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-04-23 01:27:18 +0200

    Layout changes for sw400 and sw600

--- app/src/main/java/sk3m3l1io/duisburg/memogame/controller/menu/MainActivity.java
+++ app/src/main/java/sk3m3l1io/duisburg/memogame/controller/menu/MainActivity.java
@@ -202,22 +202,20 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onTimeModeClick() {
-//        if (GoogleSignIn.getLastSignedInAccount(this) != null) {
-//            addGameBriefingFragment(gameMode = GameMode.TIME);
-//        } else {
-//            showSignInSnackbar();
-//        }
+        if (GoogleSignIn.getLastSignedInAccount(this) != null) {
             addGameBriefingFragment(gameMode = GameMode.TIME);
+        } else {
+            showSignInSnackbar();
+        }
     }
 
     @Override
     public void onSurvivalModeClick() {
-//        if (GoogleSignIn.getLastSignedInAccount(this) != null) {
-//            addGameBriefingFragment(gameMode = GameMode.SURVIVAL);
-//        } else {
-//            showSignInSnackbar();
-//        }
+        if (GoogleSignIn.getLastSignedInAccount(this) != null) {
             addGameBriefingFragment(gameMode = GameMode.SURVIVAL);
+        } else {
+            showSignInSnackbar();
+        }
     }
 
     private void showSignInSnackbar() {
