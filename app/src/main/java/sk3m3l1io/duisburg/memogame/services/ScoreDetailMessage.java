9b18a7598e8cc1d0d0f1b425dec57024fb8bec06
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2020-04-22 16:06:33 +0200

    Added german detail message

--- app/src/main/java/sk3m3l1io/duisburg/memogame/services/ScoreDetailMessage.java
+++ app/src/main/java/sk3m3l1io/duisburg/memogame/services/ScoreDetailMessage.java
@@ -6,6 +6,7 @@ import sk3m3l1io.duisburg.memogame.model.pojos.ScoreData;
 
 public final class ScoreDetailMessage {
     private static final String GREEK = "ell";
+    private static final String GERMAN = "deu";
 
     private final int rank;
     private final ScoreData sd;
@@ -20,7 +21,8 @@ public final class ScoreDetailMessage {
         switch (language) {
             case GREEK:
                 return getGreekMessage();
-            // TODO: implement a german message
+            case GERMAN:
+                return getGermanMessage();
             default:
                 return getDefaultMessage();
         }
@@ -45,11 +47,11 @@ public final class ScoreDetailMessage {
 
     private String getSecondSentence() {
         if (sd.isTimeCompleted() && sd.isSurvivalCompleted()) {
-            return "He/She gained honors because of excellence shown in Time and Survival modes.";
+            return "He/She gained honors because of excellence shown in \"Time\" and \"Survival\" modes.";
         } else if (sd.isTimeCompleted() && !sd.isSurvivalCompleted()) {
-            return "He/She gained an honor because of excellence shown in Time mode.";
+            return "He/She gained an honor because of excellence shown in \"Time\" mode.";
         } else if (!sd.isTimeCompleted() && sd.isSurvivalCompleted()) {
-            return "He/She gained an honor because of excellence shown in Survival mode.";
+            return "He/She gained an honor because of excellence shown in \"Survival\" mode.";
         } else {
             return "";
         }
@@ -78,11 +80,11 @@ public final class ScoreDetailMessage {
 
     private String getSecondSentenceInGreek() {
         if (sd.isTimeCompleted() && sd.isSurvivalCompleted()) {
-            return "Κέρδισε επαίνους λόγω τέλειας επίδοσης στους γύρους Χρόνου και Επιβίωσης.";
+            return "Κέρδισε επαίνους λόγω τέλειας επίδοσης στους γύρους \"Χρόνος\" και \"Επιβίωση\".";
         } else if (sd.isTimeCompleted() && !sd.isSurvivalCompleted()) {
-            return "Κέρδισε έπαινο λόγω τέλειας επίδοσης στο γύρο Χρόνος.";
+            return "Κέρδισε έπαινο λόγω τέλειας επίδοσης στο γύρο \"Χρόνος\".";
         } else if (!sd.isTimeCompleted() && sd.isSurvivalCompleted()) {
-            return "Κέρδισε έπαινο λόγω τέλειας επίδοσης στο γύρο Επιβίωση.";
+            return "Κέρδισε έπαινο λόγω τέλειας επίδοσης στο γύρο \"Επιβίωση\".";
         } else {
             return "";
         }
@@ -90,6 +92,39 @@ public final class ScoreDetailMessage {
 
     private String getThirdSentenceInGreek() {
         return String.format("Αυτή τη στιγμή βρίσκεται στην %dη θέση.", rank);
+    }
+
+    private String getGermanMessage() {
+        return getFirstSetenceInGerman() + " " + getSecondSetenceInGerman() + " " + getThirdSetenceInGerman();
+    }
+
+    private String getFirstSetenceInGerman() {
+        String gameLiteral = "Spiele";
+        if (sd.getGamesCompleted() == 1)
+            gameLiteral = "Spiel";
+        if (sd.getAccuracyPercent() != 0) {
+            return String.format("%s beendete erfolgreich %d %s mit %.0f%% Richtigkeit.",
+                    getFirstName(), sd.getGamesCompleted(), gameLiteral, sd.getAccuracyPercent());
+        } else {
+            return String.format("%s beendete erfolgreich %d %s.",
+                    getFirstName(), sd.getGamesCompleted(), gameLiteral);
+        }
+    }
+
+    private String getSecondSetenceInGerman() {
+        if (sd.isTimeCompleted() && sd.isSurvivalCompleted()) {
+            return "Er/Sie erhielt Auszeichnungen aufgrund der hervorragenden Leistungen in den Modi \"Zeit\" und \"Überleben.\"";
+        } else if (sd.isTimeCompleted() && !sd.isSurvivalCompleted()) {
+            return "Er/Sie erhielt eine Auszeichnung aufgrund der hervorragenden Leistung im Modus \"Zeit\".";
+        } else if (!sd.isTimeCompleted() && sd.isSurvivalCompleted()) {
+            return "Er/Sie erhielt eine Auszeichnung aufgrund der hervorragenden Leistung im Modus \"Überleben\".";
+        } else {
+            return "";
+        }
+    }
+
+    private String getThirdSetenceInGerman() {
+        return String.format("Currently ranks %d.", rank);
     }
 
     private String getFirstName() {
